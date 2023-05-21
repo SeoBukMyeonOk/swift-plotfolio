97bda26cb111723ca86afe8c7f220c497c851318
송영모 <mooyoung2309@gmail.com>
2023-05-21 14:57:33 +0900

    feat: 수정 화면 구현

---
+++ plotfolio/Core/PlotType.swift
@@ -0,0 +1,20 @@
+//
+//  PlotType.swift
+//  plotfolio
+//
+//  Created by 송영모 on 2023/05/21.
+//
+
+import Foundation
+
+enum PlotType: String, CaseIterable {
+    case movie
+    case book
+    case exhibition
+    case concert
+    case music
+    case food
+    case place
+    case media
+    case etc = "etc."
+}
--- plotfolio/Feature/EditPlot/EditPlotView.swift
+++ plotfolio/Feature/EditPlot/EditPlotView.swift
@@ -11,12 +11,66 @@ import ComposableArchitecture
 
 struct EditPlotView: View {
     public let store: StoreOf<EditPlotStore>
+    @State private var text: String = ""
+    @State var name: String = ""
     
     public var body: some View {
         WithViewStore(self.store, observe: { $0 }) { viewStore in
-            VStack {
-                Text("메모")
+            VStack(spacing: .zero) {
+                HStack(alignment: .firstTextBaseline) {
+                    VStack(alignment: .leading, spacing: 10) {
+                        Button("Date") {
+                            
+                        }
+                        .foregroundColor(.black)
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        
+                        TextField("Title", text: $name)
+                            .padding(.bottom)
+                        
+                        HStack(spacing: .zero) {
+                            Image(systemName: "star")
+                            Image(systemName: "star")
+                            Image(systemName: "star")
+                            Image(systemName: "star")
+                            Image(systemName: "star")
+                        }
+                    }
+                    
+                    Spacer()
+                    
+                    VStack(alignment: .leading) {
+                        ForEach(PlotType.allCases, id: \.self) { type in
+                            HStack {
+                                Button(
+                                    action: {},
+                                    label: {
+                                        Image(systemName: "circle")
+                                            .imageScale(.small)
+                                            .font(.footnote)
+                                    })
+                                
+                                Text("\(type.rawValue)")
+                                    .font(.footnote)
+                                    .fontWeight(.medium)
+                            }
+                        }
+                    }
+                }
+                .padding()
+                
+                Divider()
+                    .padding(.horizontal)
+                
+                VStack {
+                    TextEditor(text: $text)
+                        .padding()
+                        .foregroundColor(.black)
+                        .lineSpacing(5)
+                }
             }
+            .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
